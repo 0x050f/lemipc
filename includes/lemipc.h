3ad2b1d17e044b0b6af31923d08427b59ba4a112
ska42 <skalyxe00@gmail.com>
2022-08-02 14:41:34 +0200

    first memshare

--- includes/lemipc.h
+++ includes/lemipc.h
@@ -1,6 +1,23 @@
 #ifndef LEMIPC_H
 # define LEMIPC_H
 
-#include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/mman.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
+# define PRG_NAME "lemipc"
+# define MEMORY_SIZE 4096
+
+typedef struct	s_lemipc {
+	int			fd;
+	size_t		size;
+	void		*addr;
+}				t_lemipc;
 
 #endif
--- srcs/lemipc.c
+++ srcs/lemipc.c
@@ -1,7 +1,101 @@
 #include "lemipc.h"
 
-int			main(void)
+t_lemipc	g_lemipc;
+
+size_t		align_up(size_t size, size_t align) {
+	return (size + (align - (size % align)));
+}
+
+int			create_game(int fd)
 {
-	printf("Hello world !\n");
-	return (0);
+	size_t	size = size = align_up(MEMORY_SIZE, getpagesize());
+	if (ftruncate(fd, size) < 0)
+	{
+		dprintf(STDERR_FILENO, "%s: ftruncate(): %s\n", PRG_NAME, strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	return (size);
+}
+
+int			join_game(int fd, size_t size, char *team_name)
+{
+	(void)team_name;
+	void *addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (addr == MAP_FAILED)
+	{
+		dprintf(STDERR_FILENO, "%s: mmap(): %s\n", PRG_NAME, strerror(errno));
+		return (EXIT_FAILURE);
+	}
+//	for (size_t i = 0; i < size; i++)
+//		printf("%02x", ((char *)addr)[i]);
+//	printf("\n");
+//	sprintf(addr, "Bonjour bonjour");
+	sleep(5);
+	//
+	if (munmap(addr, size) < 0)
+	{
+		dprintf(STDERR_FILENO, "%s: munmap(): %s\n", PRG_NAME, strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+void		signal_handler(int signum)
+{
+	(void)signum;
+	if (g_lemipc.addr)
+		munmap(g_lemipc.addr, g_lemipc.size);
+	if (g_lemipc.fd >= 0)
+		close(g_lemipc.fd);
+	shm_unlink(PRG_NAME);
+	printf("\b\bLeaving the game...\n");
+	exit(EXIT_SUCCESS);
+}
+
+int			lemipc(char *team_name)
+{
+	int		ret;
+	struct	stat sb;
+
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+	{
+		dprintf(STDERR_FILENO, "%s: signal(): %s\n", PRG_NAME, strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	if ((g_lemipc.fd = shm_open(PRG_NAME, O_RDWR | O_CREAT, 0644)) < 0)
+	{
+		dprintf(STDERR_FILENO, "%s: shm_open(): %s\n", PRG_NAME, strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	if (fstat(g_lemipc.fd, &sb) < 0)
+	{
+		dprintf(STDERR_FILENO, "%s: fstat(): %s\n", PRG_NAME, strerror(errno));
+		ret = EXIT_FAILURE;
+		goto end;
+	}
+	if (!sb.st_size)
+	{
+		if ((ret = create_game(g_lemipc.fd)) == EXIT_FAILURE)
+			goto end;
+		ret = join_game(g_lemipc.fd, ret, team_name);
+	}
+	else
+		ret = join_game(g_lemipc.fd, sb.st_size, team_name);
+end:
+	close(g_lemipc.fd);
+	shm_unlink(PRG_NAME);
+	return (ret);
+}
+
+int			main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "usage: ./%s team_name\n", PRG_NAME);
+		return (EXIT_FAILURE);
+	}
+	g_lemipc.fd = -1;
+	g_lemipc.size = 0;
+	g_lemipc.addr = NULL;
+	return (lemipc(argv[1]));
 }
